16cf30fb07ed17f83de9ab11e28330d0b8ead877
Benjamin Chancerel <benjiroux33@gmail.com>
2020-10-04 22:17:14 +0200

    fin

--- tp_2/part_4/script_node1.sh
+++ tp_2/part_4/script_node1.sh
@@ -4,58 +4,42 @@
 
 echo "192.168.2.22 node2.tp2.b2" >> /etc/hosts
 
-#création des fichiers 
-mkdir /srv
-mkdir /srv/site1
-touch /srv/site1/index.html
-echo 'voici le site1 ya zebi' >> /srv/site1/index.html
-mkdir /srv/site2
-touch /srv/site2/index.html
-echo 'oklm le site 2 en tn' >> /srv/site2/index.html
+#ajout de l'user "web"
+useradd web -M -s /sbin/nologin
 
-#donner les perms
-chmod -R 755 /srv/site1
-chown -R vagrant:vagrant /srv/site1
+#Modifier les droits des certificats 
+chmod 400 /etc/pki/tls/private/server.key
+chown web:web /etc/pki/tls/private/server.key
+chmod 444 /etc/pki/tls/certs/server.crt
+chown web:web /etc/pki/tls/certs/server.crt
 
-chmod -R 755 /srv/site2
-chown -R vagrant:vagrant /srv/site2
-
+#ajouter lesservices http et https
 firewall-cmd --zone=public --add-service=http
 firewall-cmd --zone=public --add-service=https
-firewall-cmd --reload
 
-#config nginx
+#Configuration de NGINX
 echo "worker_processes 1;
-error_log nginx_error.log;
+error_log /var/log/nginx/error.log;
 events {
     worker_connections 1024;
 }
 
+pid /run/nginx.pid;
+user web;
+
 http {
-     server {
+    server {
         listen 80;
-
         server_name node1.tp2.b2;
-
-        location / {
-                return 301 /site1;
-        }
-
-        location /site1 {
-                alias /srv/site1;
-        }
-
-        location /site2 {
-                alias /srv/site2;
-        }
-}
+        return 301 https://$host$request_uri;
+    }
 
 server {
         listen 443 ssl;
-
         server_name node1.tp2.b2;
-        ssl_certificate server.crt;
-        ssl_certificate_key server.key;
+
+        ssl_certificate /etc/pki/tls/certs/server.crt;
+        ssl_certificate_key /etc/pki/tls/private/server.key;
 
         location / {
             return 301 /site1;
@@ -64,17 +48,41 @@ server {
         location /site1 {
             alias /srv/site1;
         }
+
         location /site2 {
             alias /srv/site2;
         }
     }
-}" >> /etc/nginx/nginx.conf
+}
+" >> /etc/nginx/nginx.conf
 
-#certificat
+# création des dossiers avec les fichiers 
+mkdir /srv
+mkdir /srv/site1
+touch /srv/site1/index.html
+chown web:web /srv/site1/index.html
+chmod 550 /srv/site1
+chmod 440 /srv/site1/index.html
+echo 'site 1' | tee /srv/site1/index.html
+
+mkdir /srv/site2
+touch /srv/site2/index.html
+chown web:web /srv/site2/index.html
+chmod 550 /srv/site2
+chmod 440 /srv/site2/index.html
+echo 'site 2' | tee /srv/site2/index.html
+
+#certificat 
 openssl req -new -newkey rsa:2048 -days 365 -nodes -x509 -keyout server.key -out server.crt -subj "/C=GB/ST=London/L=London/O=Global Security/OU=IT Department/CN=node1.tp2.b2"
 
 mv server.crt /etc/nginx
 mv server.key /etc/nginx
 
-sudo systemctl start nginx
+systemctl start nginx
 
+# installation netdata
+bash <(curl -Ss https://my-netdata.io/kickstart.sh) --dont-wait
+
+sudo firewall-cmd --add-port=19999/tcp --permanent
+
+sudo firewall-cmd --reload
--- tp_2/part_4/script_node2.sh
+++ tp_2/part_4/script_node2.sh
@@ -4,5 +4,7 @@
 
 echo "192.168.2.21 node1.tp2.b2" >> /etc/hosts
 
+systemctl start firewalld
+
 firewall-cmd --zone=public --add-service=http
 firewall-cmd --zone=public --add-service=https
